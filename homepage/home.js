b8afd38b6c4afd784c0990c46d1861c60f733dbf
nmpratik <nmprathik@gmail.com>
2023-05-03 15:49:37 +0530

    home js

---
+++ homepage/home.js
@@ -0,0 +1,11 @@
+const homeLink = document.getElementById("evmss");
+homeLink.addEventListener("click", reloadPage);
+function reloadPage() {
+  window.location.reload();
+}
+
+document.getElementById("login").addEventListener("click", function() {
+  window.location.href = "../login/login-page.html";
+});
+
+
