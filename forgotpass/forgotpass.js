641df5e661c0281fff163995660f669923245f52
nmpratik <nmprathik@gmail.com>
2023-05-04 14:18:41 +0530

    forgotpass

--- forgotpass/forgotpass.js
+++ forgotpass/forgotpass.js
@@ -1,9 +1,27 @@
 
-const forgotPasswordForm = document.getElementById("forgotpassword");
+const forgotPasswordForm = document.getElementById("forgot-password-form");
 
 forgotPasswordForm.addEventListener("submit", function (event) {
     event.preventDefault();
+    const formData = new FormData(event.target);
+    const user = Object.fromEntries(formData.entries())
+    console.log(user)
+    validateForm(user)
+    // Make HTTP POST request to REST API endpoint
+    axios.post('http://localhost:8080/user/login/forgotpass', user)
+        .then((response) => {
+
+            console.log(response.data);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
     const message = "Password reset link has been sent to your email!";
     alert(message);
-    window.location.href = "../login/login-page.html";
+    window.location.href = "../loginpage/login.html";
 });
+
+const validateForm = ({ email }) => {
+    if (email.length <= 0) return { msg: 'invalid email', sts: false }
+    return { sts: 'success', msg: 'all fields are valid' }
+}
